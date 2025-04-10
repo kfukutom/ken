fc05c9698ecc10644d402484709dbd6f4c20b38a
Kensuke Fukutomi <145524808+kfukutom@users.noreply.github.com>
2025-04-10 17:09:00 -0400

    Device Compatible

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -1,60 +1,74 @@
 import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="animate-fade-left animate-once animate-ease-linear bg-white px-4 sm:px-12 md:px-24 lg:px-36 xl:px-[424px] py-12 space-y-6">
-        {/* I fixed everything here*/}
-        <p className="text-left text-xl text-yellow-600">design + beyond</p>
+    <div className="animate-fade-left animate-once animate-ease-linear bg-white px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-12 space-y-6 max-w-screen-md mx-auto">
+      <p className="text-left text-xl text-yellow-600">design + beyond</p>
 
-        <div className="space-y-2">
-            <p className="font-medium">i. urban screens | <span className="text-sm text-gray-500"> Figma, Illustrator, RAWGraphs</span></p>
-            <p className="font-thin text-gray-500">
-            Deconstructing a tangible urban object to uncover the systems and modules that exist beyond its immediate context.
-            </p>
-            <a href="/design/urban-screens" className="text-blue-600 underline hover:text-blue-800 transition">
-            view project
-            </a>
-        </div>
+      <div className="space-y-2">
+        <p className="font-medium">
+          i. urban screens | <span className="text-sm text-gray-500">Figma, Illustrator, RAWGraphs</span>
+        </p>
+        <p className="font-thin text-gray-500">
+          Deconstructing a tangible urban object to uncover the systems and modules that exist beyond its immediate context.
+        </p>
+        <a href="/design/urban-screens" className="text-blue-600 underline hover:text-blue-800 transition">
+          view project
+        </a>
+      </div>
 
-        <hr className="border-gray-200" />
+      <hr className="border-gray-200" />
 
-        <div className="space-y-2">
-            <p className="font-medium">ii. digital zine | <span className="text-sm text-gray-500">Figma, Miro</span></p>
-            <p className="text-gray-400 font-extralight text-xs">Partner: Kaylee</p>
-            <p className="font-thin text-gray-500">Designed an digital magazine outling various character archetypes of an observed environment.</p>
-            <a href="/design/zine" className="text-blue-600 underline hover:text-blue-800 transition">
-            view project
-            </a>
-        </div>
+      <div className="space-y-2">
+        <p className="font-medium">
+          ii. digital zine | <span className="text-sm text-gray-500">Figma, Miro</span>
+        </p>
+        <p className="text-gray-400 font-extralight text-xs">Partner: Kaylee</p>
+        <p className="font-thin text-gray-500">
+          Designed a digital magazine outlining various character archetypes of an observed environment.
+        </p>
+        <a href="/design/zine" className="text-blue-600 underline hover:text-blue-800 transition">
+          view project
+        </a>
+      </div>
 
-        <hr className="border-gray-200" />
+      <hr className="border-gray-200" />
 
-        <div className="space-y-2">
-            <p className="font-medium">iii. mobile app wireframe | <span className="text-sm text-gray-500">Figma, Sketch, UXPin</span></p>
-            <p className="text-gray-400 font-extralight text-xs">Partner: Georgia, Jessica</p>
-            <p className="font-thin text-gray-500">I developed a wireframe for an ecommerce business.</p>
-            <a href="/design/urban-screens" className="text-blue-600 underline hover:text-blue-800 transition">
-            view project
-            </a>
-        </div>
+      <div className="space-y-2">
+        <p className="font-medium">
+          iii. mobile app wireframe | <span className="text-sm text-gray-500">Figma, Sketch, UXPin</span>
+        </p>
+        <p className="text-gray-400 font-extralight text-xs">Partner: Georgia, Jessica</p>
+        <p className="font-thin text-gray-500">
+          I developed a wireframe for an ecommerce business.
+        </p>
+        <a href="/design/urban-screens" className="text-blue-600 underline hover:text-blue-800 transition">
+          view project
+        </a>
+      </div>
 
-        <hr className="border-gray-200" />
-        <div className="space-y-2">
-            <p className="font-medium">iv. think technologies | <span className="text-sm text-gray-500">LucidChart, Figma</span></p>
-            <p className="text-gray-400 font-extralight text-xs">Co-lead: Jonas</p>
-            <p className="font-thin text-gray-500">Diagramming a backend architecture for an ambitious Detroit startup.</p>
-            <a href="/design/urban-screens" className="text-blue-600 underline hover:text-blue-800 transition">
-            view project
-            </a>
-        </div>
+      <hr className="border-gray-200" />
 
-        <hr className="border-gray-200" />
-        <Link href="/" className="block w-fit mt-4">
-            <button className="text-sm px-4 py-2 cursor-pointer bg-yellow-600 text-white rounded-xl font-light hover:bg-yellow-700 transition duration-200 shadow-md hover:shadow-xl">
-            Return Home
-            </button>  
-        </Link>
+      <div className="space-y-2">
+        <p className="font-medium">
+          iv. think technologies | <span className="text-sm text-gray-500">LucidChart, Figma</span>
+        </p>
+        <p className="text-gray-400 font-extralight text-xs">Co-lead: Jonas</p>
+        <p className="font-thin text-gray-500">
+          Diagramming a backend architecture for an ambitious Detroit startup.
+        </p>
+        <a href="/design/urban-screens" className="text-blue-600 underline hover:text-blue-800 transition">
+          view project
+        </a>
+      </div>
 
-        
+      <hr className="border-gray-200" />
+
+      <Link href="/" className="block w-fit mt-4">
+        <button className="text-sm px-4 py-2 cursor-pointer bg-yellow-600 text-white rounded-xl font-light hover:bg-yellow-700 transition duration-200 shadow-md hover:shadow-xl">
+          Return Home
+        </button>
+      </Link>
     </div>
   );
-} // Page.tsx()
+} // Home().tsx
--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,17 @@ import ExperienceList from "@/components/Experience";
 
 export default function Home() {
   return (
-    <div className="animate-fade-up bg-white animate-once animate-ease-linear px-4 sm:px-12 md:px-24 lg:px-36 xl:px-[424px] py-12">
+    <div className="animate-fade-up animate-once animate-ease-linear bg-white px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-12 max-w-screen-md mx-auto">
       <Header />
       <Bio />
       <SocialLinks />
       <ProjectList />
       <ExperienceList />
-      <a className="underline text-sm cursor-pointer text-blue-600 hover:text-blue-500" href="/about">More about me.</a>
+      <a className="underline text-sm cursor-pointer text-blue-600 hover:text-blue-500" href="/about">
+        More about me.
+      </a>
       <p className="pt-20 text-center text-xs text-gray-600">Built by Ken with Typescript/Tailwind</p>
       <p className="pt-1 text-center text-xs text-yellow-600">let's create art 🖤</p>
     </div>
   );
-} // Index or Page.tsx()
+}
